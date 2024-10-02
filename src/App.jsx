34240f4cb17d120297674cbadfcaec88556697f5
franz-Kosy <franzmesh5@gmail.com>
2024-10-02 05:47:41 +0100

    functional Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,111 @@
-import {FaLinkedin, FaGithub, FaTwitter, FaYoutube, FaYahoo} from 'react-icons/fa';
+import {FaLinkedin, FaGithub, FaTwitter, FaMailBulk} from 'react-icons/fa';
 import './App.css';
-// import gsap from "gsap";
-// import {useGSAP} from "@gsap/react";
+import { useState } from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function App() {
 
-  let hamburger = require("./hamburger.png");
+  /*******************  ProjectImg Side start******************/
+  let logo = require("./Mylogo.png");
+  let getLinkedProject = require("./getLinked.png");
+  let ecommerceProject = require("./ecommerce.png");
+  let glintProject = require("./glint1.png");
+  let netFlixProject = require("./netFlix.png");
+  /*******************  ProjectImg Side end ******************/
 
-  function show(){
-    console.log("Clicked");
-  }
+  /****************** Hamburger Toggle Mobile  ***************/
+  const [isOpen, setisOpen] = useState(false);
+  const show = () => setisOpen(!isOpen);
+   /************** Hamburger Toggle Mobile  end***************/
 
-  return(
+  /******************** Form Handling ************************/
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    <div className="container">
-        <header>
-          <div id='logoContainer'>
-            <a href="#">Koko</a>
-          </div>
+    emailjs
+      .sendForm(
+        'service_ppu7uih',
+        'template_9rwonyg',
+        form.current, {
+        publicKey: 'g6_U8pehgOxx6JpKM',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          form.value = '';
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
 
-          <div id='hamburger'>
-            <img src={hamburger} alt="Hamburger Icon"onClick={show}/>
-          </div>
+return(
+<div className="container" id='Home'>
+  <header>
+    <div id='logoContainer'>
+      <a href="#container"><img src={logo} alt="Logo" id='logo'/></a>
+      <span>KOKO.</span>
+    </div>
+
+    <div id='hamburgerContainer' className={`hamburger-icon ${isOpen ? "open" : ""}`} onClick={show}>
+      <div id="hamburger"></div>
+
+      <div className={`nav-menu ${isOpen ? "show" : ""}`}>
+        <ol>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#project">Project</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ol>
+      </div>
+    </div>
 
           <nav>
-            <ul>
-              <li>HOME</li>
-              <li>ABOUT</li>
-              <li>PROJECTS</li>
-              <li>CONTACT</li>
+            <ul> 
+              <li><a href="#Home">HOME</a></li>
+              <li><a href="#about">ABOUT</a></li>
+              <li><a href="#project">PROJECTS</a></li>
+              <li><a href="#contact">CONTACT</a></li>
             </ul>
           </nav>
         </header>
 
       <main>
 
+      <div className='iconSideContainer'>
+            <a href="https://www.linkedin.com/in/femi-5b6829267" target='blank' rel="noreferrer">
+                <FaLinkedin  color='white' className='icon'/>
+            </a>
+            <a href="https://github.com/Kosisochii" target='_blank' rel="noreferrer">
+              <FaGithub  color="white" className='icon'/>
+            </a>
+            <a href="https://x.com/kosiso03" target='_blank' rel="noreferrer">
+              <FaTwitter color="white" className='icon'/>
+            </a>
+            <a href="mailto:kosisofrancis03@gmail.com" target='_blank' rel="noreferrer">
+              <FaMailBulk color="white" className='icon'/>
+            </a>
+        </div>
+
         <section id='title'>
 
           <h1>Welcome</h1>
-          <h2>HI, I'M KOSISO EGUNGWU</h2>
+          <h2>I'M KOSISO EGUNGWU</h2>
           <p>A Result-Oriented Software Engineering and Full-Stack Developer.</p>
 
-          <button>PROJECTS</button>
+          <a href="#project">
+            <button>PROJECTS</button>
+          </a>
+
+          <div className='mouseScroll'>
+            <div id='mouse'></div>
+          </div>
         </section>
 
-        <section className ="about">
+        <section className ="about" id='about'>
 
           <div id='aboutContainer'>
               <h3>ABOUT ME</h3>
@@ -88,30 +149,83 @@ function App() {
           </div>
         </section>
 
-        {/* <section id="project">
-          <h3>PROJECTS</h3>
-          <p>Here you will find some of the personal and clients projects that i created with each project containing its own case study</p>
-        </section> */}
+        <section id="project">
+          <div className='project'>
+            <h3>PROJECTS</h3>
+            <p>Here you will find some of the personal and clients projects that i created with each project containing its own case study</p>
+          </div>
 
-        <div className='contactContainer'>
+
+          <div className='projectContainer'>
+            <div className='projectImgContainer'>
+              <img src={ecommerceProject} alt="Ecommerce Project"/>
+            </div>
+            <div className='projectContent'>
+              <h4>GetLinked Project</h4>
+              <p>Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally</p>
+              <button>CASE STUDY</button>
+            </div>
+
+          </div>
+
+          <div className='projectContainer'>
+            <div className='projectImgContainer'>
+               <img src={getLinkedProject} alt="Getlinked Site"/>
+            </div>
+
+            <div className='projectContent'>
+              <h4>Glint Project</h4>
+              <p>Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally</p>
+              <button>CASE STUDY</button>
+            </div>
+          </div>
+
+
+          <div className='projectContainer'>
+
+            <div className='projectImgContainer'>
+              <img src={glintProject} alt="Glint Project"/>
+            </div>
+            <div className='projectContent'>
+              <h4>E-commerce Site</h4>
+              <p>Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally</p>
+              <button>CASE STUDY</button>
+            </div>
+
+          </div>
+
+          <div className='projectContainer'>
+            <div className='projectImgContainer'>
+              <img src={netFlixProject} alt="Netflix Project"/>
+            </div>
+            <div className='projectContent'>
+              <h4>NetFlix Project</h4>
+              <p>Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally</p>
+              <button>CASE STUDY</button>
+            </div>
+
+          </div>
+        </section>
+
+        <div className='contactContainer' id='contact'>
         <section className='contactSection'>
           <div>
             <h3>CONTACT</h3>
             <p>Feel free to Contact me by submitting the form below and i will get back to you as soon as possible.</p>
 
             <div id='formContainer'>
-              <form>
-                <label htmlFor="name">Name</label>
-                <input type="text" name='name' placeholder='Enter Your Name'/>
+              <form ref={form} onSubmit={sendEmail}>
+                <label htmlFor="sender_name">Name</label>
+                <input type="text" name='sender_name' placeholder='Enter Your Name' required/>
 
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" placeholder='Enter Your Email'/>
+                <label htmlFor="sender_email">Email</label>
+                <input type="email" name="sender_email" placeholder='Enter Your Email' required/>
 
                 <label htmlFor="message">Message</label>
-                <textarea name="message" placeholder='Enter Your Message'></textarea>
+                <textarea name="message" placeholder='Enter Your Message' required></textarea>
 
                 <p id="submitContainer">
-                  <button type="submit">SUBMIT</button>
+                  <button type="submit" name='Name'>SUBMIT</button>
                 </p>
               </form>
             </div>
@@ -129,15 +243,18 @@ function App() {
             <h3>SOCIAL</h3>
 
             <div id='iconContainer'>
-                <a href="www.linkedin.com/in/femi-5b6829267">
-                  <FaLinkedin  color='white' className='icon'/>
-                </a>
-                <FaGithub  color="white" className='icon'/>
-                <FaTwitter color="white" className='icon'/>
-                <FaYoutube color="white" className='icon'/>
-                <a href="mailto:kosisofrancis03@gmail.com">
-                  <FaYahoo color="white" className='icon'/>
-                </a>
+            <a href="www.linkedin.com/in/femi-5b6829267" target='blank' rel="noreferrer">
+                <FaLinkedin  color='white' className='icon'/>
+            </a>
+            <a href="https://github.com/Kosisochii" target='_blank' rel="noreferrer">
+              <FaGithub  color="white" className='icon'/>
+            </a>
+            <a href="https://x.com/kosiso03" target='_blank' rel="noreferrer">
+              <FaTwitter color="white" className='icon'/>
+            </a>
+            <a href="mailto:kosisofrancis03@gmail.com" target='_blank' rel="noreferrer">
+              <FaMailBulk color="white" className='icon'/>
+            </a>
             </div>
           </div>
 
